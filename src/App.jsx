7fa49c6c441cd132f7d348c09ec64f39d725d010
Sadhana <sadhanarameshpal@gmail.co>
2026-03-27 20:24:58 +0530

    restore real app

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,36 @@
-// import React from 'react'
-// import { Link, Route, Routes, useLocation } from 'react-router-dom';
-// import Home from './components/Home';
-// import Details from './components/Details';
-// import Create from './components/Create';
-// import Edit from './components/Edit';
-
-// const App = () => {
-
-//    const{search, pathname} = useLocation();
-
-//   return (
-//     <div className='h-screen w-screen flex'>
-//        {(pathname != '/' || search.length > 0) && (
-
-//        <Link
-//         to="/" 
-//         className='text-red-300 absolute left-[17%] top-[3%]'>
-//           Home
-//         </Link>
-//       )}
-
-
-//      <Routes>
-//         <Route path='/' element={<Home/>}/>
-//        <Route path='/create' element={<Create/>}/>
-//        <Route path='/details/:id' element={<Details/>}/>
-//        <Route path='/edit/:id' element={<Edit/>}/>
-        
-       
-//       </Routes>  
-//       </div>
-//   );
-// };
-
-// export default App;
 import React from 'react'
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
+import Home from './components/Home';
+import Details from './components/Details';
+import Create from './components/Create';
+import Edit from './components/Edit';
 
 const App = () => {
-  return <h1>Hello Sadhana 🚀</h1>
-}
+
+   const{search, pathname} = useLocation();
+
+  return (
+    <div className='h-screen w-screen flex'>
+       {(pathname != '/' || search.length > 0) && (
+
+       <Link
+        to="/" 
+        className='text-red-300 absolute left-[17%] top-[3%]'>
+          Home
+        </Link>
+      )}
+
+
+     <Routes>
+        <Route path='/' element={<Home/>}/>
+       <Route path='/create' element={<Create/>}/>
+       <Route path='/details/:id' element={<Details/>}/>
+       <Route path='/edit/:id' element={<Edit/>}/>
+        
+       
+      </Routes>  
+      </div>
+  );
+};
 
 export default App;
